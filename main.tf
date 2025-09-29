6fcca32805906319c5b90e225900a4661bd800e4
Leonardo Montiel <lmontiel@atlassian.com>
2025-09-29 16:48:50 -0300

    adding alb again

--- main.tf
+++ main.tf
@@ -42,14 +42,17 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
-  
-  name            = "blog-alb"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -57,19 +60,13 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        main_target = {
-          target_id      = aws_instance.blog.id
-          port           = 80
-        }
-      }
     }
   ]
 
-  http_tcp_listener = [
+  http_tcp_listeners = [
     {
       port               = 80
-      protocol           = http
+      protocol           = "HTTP"
       target_group_index = 0
     }
   ]
